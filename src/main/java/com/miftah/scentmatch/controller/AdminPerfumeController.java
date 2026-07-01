ea3d425f4d16ef5307626e19b6d4beb7ac31c09e
Miftahurrahman-STEI <mippganz@gmail.com>
2026-07-02 02:04:25 +0700

    fix: save data perfume

--- src/main/java/com/miftah/scentmatch/controller/AdminPerfumeController.java
+++ src/main/java/com/miftah/scentmatch/controller/AdminPerfumeController.java
@@ -1,9 +1,14 @@
 package com.miftah.scentmatch.controller;
 
+import com.miftah.scentmatch.entity.Brand;
+import com.miftah.scentmatch.entity.Category;
 import com.miftah.scentmatch.entity.Perfume;
 import com.miftah.scentmatch.service.BrandService;
 import com.miftah.scentmatch.service.CategoryService;
 import com.miftah.scentmatch.service.PerfumeService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -34,16 +39,40 @@ public class AdminPerfumeController {
     // FORM TAMBAH
     @GetMapping("/add")
     public String addPerfumeForm(Model model) {
-        model.addAttribute("perfume", new Perfume());
+
+        Perfume perfume = new Perfume();
+
+        perfume.setBrand(new Brand());
+        perfume.setCategory(new Category());
+
+        model.addAttribute("perfume", perfume);
         model.addAttribute("brands", brandService.getAllBrands());
         model.addAttribute("categories", categoryService.getAllCategories());
+        
         return "admin/perfume-form";
     }
 
     // SIMPAN PERFUME BARU
     @PostMapping("/save")
-    public String savePerfume(@ModelAttribute Perfume perfume) {
+    public String savePerfume(HttpServletRequest request, @ModelAttribute Perfume perfume) {
+
+        System.out.println("===========");
+        System.out.println("Brand      : " + perfume.getBrand());
+        System.out.println("Category   : " + perfume.getCategory());
+
+        System.out.println(request.getParameter("brand.id"));
+        System.out.println(request.getParameter("category.id"));
+
+        if (perfume.getBrand() != null) {
+            System.out.println("Brand ID" + perfume.getBrand().getId());
+        }
+
+        if (perfume.getCategory() != null) {
+            System.out.println("Category ID" + perfume.getCategory().getId());
+        }
+
         perfumeService.savePerfume(perfume);
+
         return "redirect:/admin/perfumes";
     }
 
